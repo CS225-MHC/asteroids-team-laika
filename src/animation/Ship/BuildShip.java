9f81c86fd5d6676982f0390ecef3066b8b453bc4
ramisa <92804590+ramisa7@users.noreply.github.com>
2022-12-08 22:46:19 -0500

    Ship Object done

---
+++ src/animation/Ship/BuildShip.java
@@ -0,0 +1,198 @@
+package animation.Ship;  
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.util.Random;
+
+import animation.AbstractAnimation;
+import animation.AnimatedObject;
+
+//build the ship 
+public class BuildShip implements AnimatedObject {
+    //shape of the ship 
+    private Polygon s; 
+
+    //left edge of ship 
+    private double x ; 
+
+    //right edge of ship
+    private double y; 
+
+    private static final int SHIP_SIZE = 40 ; 
+
+    // The animation that this object is part of.
+    private AbstractAnimation animation;
+    
+    //variable to decide on the rotation amount 
+    private double rotateAmt ; 
+    
+    //generate random number to use hyperspace 
+    Random moveAmt = new Random(); 
+    private int upperBound = 600; 
+
+    //points to build the ship 
+    int[] xPoints = {-10,0,10} ; 
+    int[] yPoints = {20, -20,20};
+
+
+    /**
+     * Creates the animated object
+     * 
+     * @param animation the animation this object is part of
+     */
+    public BuildShip(AbstractAnimation animation) {
+        this.animation = animation;
+        s = new Polygon(xPoints, yPoints, 3 );
+        x = 300;  // adjusts to the screen 
+        y = 300; 
+
+    }
+
+    
+    /**
+     * Draws the triangle
+     * @param g the graphics context to draw on
+     */
+    public void paint(Graphics2D g) {
+        g.setColor(Color.BLACK);
+        g.draw(getShape());
+    }
+
+
+    /**
+     * Returns the shape after applying the current translation
+     * and rotation
+     * @return the shape located as we want it to appear
+     */
+    public Shape getShape() {
+
+        AffineTransform at1 = new AffineTransform();
+        
+        // x, y are where the origin of the shape will be: center of the window 
+        at1.translate(x, y);
+        
+        // Rotate the shape rotateAmt degrees to the direction of key-pressed
+        at1.rotate(rotateAmt);
+        
+        if(Math.abs(rotateAmt) == Math.PI*2 ){
+            rotateAmt = 0;
+        }
+    
+        nextFrame();
+
+        // Creates a ship
+        return at1.createTransformedShape(s);
+    }
+
+    /**
+     * action to take when the user clicks the up arrow button.
+     * This moves the ship straight to the direction the ship is facing
+     */
+    public void forward() {
+        //up
+        if ( rotateAmt == 0 ){
+            y = y-10;
+        }
+        //down
+        else if (rotateAmt == -Math.PI || rotateAmt == Math.PI){
+            y = y - (10*(-Math.abs(rotateAmt)/Math.PI));
+        }
+        //right
+        else if ( rotateAmt == Math.PI/2  || rotateAmt == -(3*Math.PI)/2){
+            x = x + 10; 
+        }
+        //left
+        else if ( rotateAmt == (3*Math.PI)/2 || rotateAmt == -Math.PI/2){
+            x = x - 10; 
+        }
+        //right-up
+        else if (rotateAmt == Math.PI/4 || rotateAmt == (-7*Math.PI)/4){
+            x = x +10;
+            y = y-10;
+        }
+        //right-down
+        else if(rotateAmt == (-5*Math.PI)/4 || rotateAmt == (3*Math.PI)/4){
+            x = x +10;
+            y=y+10;
+        }
+        //left-up
+        else if (rotateAmt == -Math.PI/4 || rotateAmt == (7*Math.PI)/4){
+            x=x-10;
+            y=y-10;
+        }
+        //left-down
+        else if (rotateAmt == (5*Math.PI)/4 || rotateAmt == (-3*Math.PI)/4){
+            x=x-10;
+            y=y+10;
+        }
+
+        System.out.println ("Up");
+    }
+
+    /**
+     * action to take when the user clicks the right arrow button.
+     * This rotates the ship 45degree to the right
+     */
+    public void turnRight() {
+        rotateAmt = rotateAmt + Math.PI/4;
+        System.out.println ("right");
+
+    }
+
+    /**
+     * action to take when the user clicks the left arrow button.
+     * This rotates the ship 45degree to the left
+     */
+    public void turnLeft() {
+        rotateAmt = rotateAmt + -Math.PI/4;
+        System.out.println ("left");
+
+    }
+
+    /**
+     * action to take when the user clicks the space button.
+     * This shoots a bullet towards the direction ship is facing
+     */
+    public void space() {
+        System.out.println ("Pew pew");
+    }
+
+    /**
+     * action to take when the user clicks the shift button.
+     * This launches the ship in a random coordinate in the window 
+     */
+    public void hyperspace() {
+        x = moveAmt.nextInt(upperBound); 
+        y = moveAmt.nextInt(upperBound);
+        System.out.println ("Nyoom");
+    }
+
+    /**
+     * wraps around the screen so that if a ship is on the edge,
+     * it will appear from the other end of the screen
+     */
+    @Override
+    public void nextFrame() {
+        if (x+SHIP_SIZE>animation.getWidth()){
+            x = 0;
+        }
+        else if (x<0){
+            x = animation.getWidth()-SHIP_SIZE;
+        }
+
+        if(y+SHIP_SIZE>animation.getHeight()){
+            y=0;
+        }
+        else if(y<0){
+            y = animation.getHeight()-SHIP_SIZE;
+        }
+    
+    }
+
+
+
+
+
+}
---
+++ src/animation/Ship/ShipAnimation.java
@@ -0,0 +1,130 @@
+package animation.Ship;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import animation.AbstractAnimation;
+
+
+public class ShipAnimation extends AbstractAnimation implements KeyListener{
+    // The width of the window, in pixels.
+    private static final int WINDOW_WIDTH = 600;
+    
+    // The height of the window, in pixels.
+    private static final int WINDOW_HEIGHT = 600;
+
+    private BuildShip ship = new BuildShip(this);
+    
+    private boolean moving = false; 
+
+    
+    /**
+     * Constructs an animation and initializes it to be able to accept
+     * key input.
+     */
+    public ShipAnimation () {
+        // Allow the game to receive key input
+        setFocusable(true);
+        addKeyListener (this);
+    }
+
+    /**
+     * Paint the animation by painting the objects in the animation.
+     * @param g the graphic context to draw on
+     */
+    public void paintComponent(Graphics g) {
+        // Note that your code should not call paintComponent directly.
+        // Instead your code calls repaint (as shown in the nextFrame
+        // method above, and repaint will call paintComponent.
+        
+        super.paintComponent(g);
+        ship.paint((Graphics2D) g);
+    }
+
+    
+    @Override
+    /**
+     * This is called when the user presses a key.
+     * It notifies the ship about presses of up arrow, right 
+     * arrow, left arrow, and the space bar and shift. All other keys are ignored.
+     * @param e information abou the key pressed
+     */
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        switch (key) {
+        case KeyEvent.VK_UP:
+            ship.forward();
+            break;
+        case KeyEvent.VK_RIGHT:
+            ship.turnRight();
+            break;
+        case KeyEvent.VK_LEFT:
+            ship.turnLeft();
+            break; 
+        case KeyEvent.VK_SPACE:
+            ship.space();
+            break;
+        case KeyEvent.VK_SHIFT: 
+            ship.hyperspace();
+            break; 
+        default:
+            // Ignore all other keys
+                
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void nextFrame() {
+        if (moving) {
+            ship.nextFrame();
+            repaint();
+            // if (checkCollision (shape, triangle)) {
+            //     moving = false;
+            // }
+            
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        //Create the window,
+        // set the window's title and its size.
+        JFrame f = new JFrame();
+        f.setTitle("Ship");
+        f.setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        
+        // if user closes the window, the entire program should exit.
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create the animation.
+        ShipAnimation game = new ShipAnimation();
+
+        // // Add the animation to the window
+        Container contentPane = f.getContentPane();
+        contentPane.add(game, BorderLayout.CENTER);
+
+        // Display the window.
+        f.setVisible(true);
+        
+        // Start the animation
+        game.start();
+    }
+}
