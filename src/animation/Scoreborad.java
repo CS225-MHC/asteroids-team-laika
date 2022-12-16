426f032a3e8abe8a5b719dfbffafa2dc0b91f615
ramisa <92804590+ramisa7@users.noreply.github.com>
2022-12-15 20:17:15 -0500

    Updated scoreboard, highscore

---
+++ src/animation/Scoreborad.java
@@ -0,0 +1,96 @@
+package animation;
+import java.awt.Graphics2D;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class Scoreborad extends JPanel implements AnimatedObject {
+
+    JLabel header = new JLabel();
+
+    //score
+    public int score = 0;
+
+    /*
+     * returns current score
+     */
+    public int getScore(){
+        return score; 
+    }
+    
+    /*
+     * reads the HighScore file and gets the highest score if there's any,
+     * else sets highscore to be 0
+     * returns 0 or current highest score 
+     */
+    public int getHighScore(){
+        int highS = 0 ; 
+        
+        File myFile = new File( "src/animation/HighScore.txt");
+        Scanner reader;
+        try {
+            reader = new Scanner(myFile);
+            
+            if (reader.hasNext()){  // file is not empty 
+                String line="" ; 
+                while( reader.hasNextLine()){  
+                    line = reader.nextLine();
+                }
+                String lastScore = line.split(": ")[line.split(": ").length -1]; 
+                highS = Integer.parseInt(lastScore);
+                return highS; 
+                                    
+            }else{
+                highS = 0 ; 
+            }
+            
+        }catch (FileNotFoundException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        };    
+        return highS; 
+
+    }
+
+    /**
+    * updates the score dynamically and shows in the window
+    * every time a large asteroid is hit, score increases by 20.   
+    */
+    public void increaseScore(){
+        score = score + 20; 
+        this.header.setText("<html><h2>HighScore: " + getHighScore() +"     Score: "+ getScore() +"</h2></html>"); 
+    }
+    /*
+     * construct the scoreboard 
+    */
+    public Scoreborad(){
+
+        // Add all the GUI elements to the display.
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));        
+        header.setText("<html><h2>HighScore: " + getHighScore() +"     Score: "+ getScore() +"</h2></html>");
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+        add(header); 
+    
+    }
+
+    @Override
+    public void paint(Graphics2D g) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void nextFrame() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
+
+
+
+
+  
--- src/animation/Ship/GameAnimation.java
+++ src/animation/Ship/GameAnimation.java
@@ -3,23 +3,42 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import animation.AbstractAnimation;
 import animation.Bullet;
+<<<<<<< Updated upstream:src/animation/Ship/GameAnimation.java
 import animation.Asteroids;
 
 
 
 public class GameAnimation extends AbstractAnimation implements KeyListener{
+=======
+import animation.Scoreborad;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.awt.event.WindowAdapter;
+
+
+public class ShipAnimation extends AbstractAnimation implements KeyListener, WindowListener  {
+>>>>>>> Stashed changes:src/animation/Ship/ShipAnimation.java
     // The width of the window, in pixels.
     private static final int WINDOW_WIDTH = 600;
     
     // The height of the window, in pixels.
     private static final int WINDOW_HEIGHT = 600;
+<<<<<<< Updated upstream:src/animation/Ship/GameAnimation.java
 
     // Create new ship
     private BuildShip ship = new BuildShip(this);
@@ -62,6 +81,24 @@ public class GameAnimation extends AbstractAnimation implements KeyListener{
     public Asteroids[] getAsteroids(){
         return asteroidList;
     }
+=======
+    
+    //builidng the ship 
+    private BuildShip ship = new BuildShip(this);
+    
+    //Create the scoreborad 
+    public static Scoreborad scoreB  = new Scoreborad(); 
+
+    //building the bullet     private Bullet bullet ;
+    private Bullet bullet ;  
+
+    private int j = 0;
+
+
+    private boolean moving = false; //for ship 
+    private boolean bulletMoving = false; //bullte
+  
+>>>>>>> Stashed changes:src/animation/Ship/ShipAnimation.java
     
     /**
      * Constructs an animation and initializes it to be able to accept
@@ -70,10 +107,16 @@ public class GameAnimation extends AbstractAnimation implements KeyListener{
     public GameAnimation () {
         // Allow the game to receive key input
         setFocusable(true);
+<<<<<<< Updated upstream:src/animation/Ship/GameAnimation.java
         addKeyListener (this);
         //need to change name of the constructor
         setAsteroids();
+=======
+        addKeyListener ((KeyListener) this);
+
+>>>>>>> Stashed changes:src/animation/Ship/ShipAnimation.java
     }
+
 
     /**
      * Paint the animation by painting the objects in the animation.
@@ -82,11 +125,18 @@ public class GameAnimation extends AbstractAnimation implements KeyListener{
     public void paintComponent(Graphics g) {
 
         super.paintComponent(g);
+<<<<<<< Updated upstream:src/animation/Ship/GameAnimation.java
         if(shipVisible){
             ship.paint((Graphics2D) g);
             if ( bulletMoving){
                 bullet.paint((Graphics2D) g);
            }
+=======
+        ship.paint((Graphics2D) g);
+        if ( bulletMoving){
+            bullet.paint((Graphics2D) g);
+
+>>>>>>> Stashed changes:src/animation/Ship/ShipAnimation.java
         }
 
         for(int i = 0; i <asteroidList.length ; i++){
@@ -119,9 +169,10 @@ public class GameAnimation extends AbstractAnimation implements KeyListener{
             ship.turnLeft();
             break; 
         case KeyEvent.VK_SPACE:
-            bulletMoving = true; 
+            bulletMoving = true;
             bullet = new Bullet(this, ship.getX(), ship.getY(), ship.getRotation());
             bullet.shoot();
+            //scoreB.increaseScore();//call score increase in case of collision 
             break;
         case KeyEvent.VK_SHIFT: 
             ship.hyperspace();
@@ -149,6 +200,7 @@ public class GameAnimation extends AbstractAnimation implements KeyListener{
          
         if (moving) {
             ship.nextFrame();
+<<<<<<< Updated upstream:src/animation/Ship/GameAnimation.java
             
         }
         if ( bulletMoving){
@@ -170,7 +222,15 @@ public class GameAnimation extends AbstractAnimation implements KeyListener{
                 shipVisible = false; // make ship disappear
                 System.out.println("HIT");
             }
+=======
+
         }
+        if ( bulletMoving){
+            bullet.nextFrame();
+
+>>>>>>> Stashed changes:src/animation/Ship/ShipAnimation.java
+        }
+
         repaint();
     }
 
@@ -193,30 +253,146 @@ public class GameAnimation extends AbstractAnimation implements KeyListener{
      */
     private boolean checkShipCollision(Asteroids shape1, BuildShip shape2){
 
+<<<<<<< Updated upstream:src/animation/Ship/GameAnimation.java
         return shape2.getShape().intersects(shape1.getShape().getBounds2D());
+=======
+    /**
+     * call this method in case of ship-asteroid collison 
+     * @return component over which displays game over on the screen 
+     */
+    public static Component gameOver(){
+        JLabel over = new JLabel(); 
+        over.setText("Game Over");
+        over.setHorizontalAlignment(SwingConstants.CENTER);
+        System.exit(2);
+        return over;
+>>>>>>> Stashed changes:src/animation/Ship/ShipAnimation.java
     }
 
     public static void main(String[] args) {
         //Create the window,
         // set the window's title and its size.
         JFrame f = new JFrame();
-        f.setTitle("Ship");
+
+        f.setTitle("Ship, Asteriods & Bullet");
         f.setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
         
-        // if user closes the window, the entire program should exit.
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         // Create the animation.
+<<<<<<< Updated upstream:src/animation/Ship/GameAnimation.java
         GameAnimation game = new GameAnimation();
+=======
+        ShipAnimation game = new ShipAnimation();
+        
+>>>>>>> Stashed changes:src/animation/Ship/ShipAnimation.java
 
         // // Add the animation to the window
         Container contentPane = f.getContentPane();
         contentPane.add(game, BorderLayout.CENTER);
-
+        contentPane.add(scoreB,  BorderLayout.BEFORE_FIRST_LINE); 
+        
         // Display the window.
-        f.setVisible(true);
+        f.setVisible(true); 
         
         // Start the animation
         game.start();
+
+        // if user closes the window, the entire program should exit.
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //before the window closes, take the score, compare it with the file's score and 
+        //write it in HighScore file if currScore> file's score  
+        f.addWindowListener(new WindowAdapter() { 
+            public void windowClosing(WindowEvent ev) {
+                // int confirmed = JOptionPane.showConfirmDialog(null, 
+                // "Are you sure you want to exit the program?", "Exit Program Message Box",
+                // JOptionPane.YES_NO_OPTION);
+        
+                // if (confirmed == JOptionPane.YES_OPTION) {
+                //     System.exit(0);
+                // }    
+                 
+                File myFile = new File( "src/animation/HighScore.txt");
+                Scanner reader;
+                try {
+                    reader = new Scanner(myFile);
+                    if (reader.hasNext()){  // file is not empty
+                        String line = "";  
+                        while( reader.hasNextLine()){                              
+                            line = reader.nextLine();
+                        }    
+
+                        String lastScore = line.split(": ")[line.split(": ").length -1]; 
+                        if ( scoreB.getScore() > Integer.parseInt(lastScore)){
+                            try {
+                                FileWriter writer = new FileWriter(myFile, true);
+                                writer.append('\n' + "New High Score: " + scoreB.getScore() ); 
+                                writer.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            } 
+                        }
+                                                
+                    }else { //if file is empty, just write
+                            
+                        try {
+                            FileWriter writer = new FileWriter(myFile,true);
+                            writer.append("New High Score: " + scoreB.getScore()); 
+                            writer.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } 
+
+                    }
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                };
+
+            }
+          });
+        
+    
+
     }
+
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        
+    }
+
+
+
+
+
+
 }
